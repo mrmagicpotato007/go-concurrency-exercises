6c96b7ec234ffb1475210d7f846bf4f181f360cb
magicpotato <mr.magicpotato007@gmail.com~>
2024-12-01 17:36:34 +0530

    implemented locking

--- 3-limit-service-time/main.go
+++ 3-limit-service-time/main.go
@@ -10,9 +10,15 @@
 
 package main
 
+import (
+	"sync"
+	"time"
+)
+
 // User defines the UserModel. Use this to check whether a User is a
 // Premium user or not
 type User struct {
+	mu        sync.Mutex
 	ID        int
 	IsPremium bool
 	TimeUsed  int64 // in seconds
@@ -21,8 +27,15 @@ type User struct {
 // HandleRequest runs the processes requested by users. Returns false
 // if process had to be killed
 func HandleRequest(process func(), u *User) bool {
+	u.mu.Lock()
+	defer u.mu.Unlock()
+	start := time.Now()
 	process()
-	return true
+	u.TimeUsed += int64(time.Since(start).Seconds())
+	if u.IsPremium {
+		return true
+	}
+	return u.TimeUsed <= 10
 }
 
 func main() {
